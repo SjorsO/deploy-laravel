19f51a937366f02a9f9fb5fe6d08004b6d00a18f
Sjors Ottjes <sjorsottjes@gmail.com>
2026-02-02 19:10:18 +0100

    run "php artisan list" only once

--- releases/gitlab/.gitlab/deployment/hooks/after-activation.sh
+++ releases/gitlab/.gitlab/deployment/hooks/after-activation.sh
@@ -2,17 +2,19 @@ php_executable=$1
 base_directory=$2
 artifacts_path=$3
 
+available_commands=$("$php_executable" artisan list)
+
 # Available since Laravel 10
-if [[ $("$php_executable" artisan list) =~ "schedule:interrupt" ]]; then
+if [[ "$available_commands" =~ "schedule:interrupt" ]]; then
     "$php_executable" artisan schedule:interrupt
 fi
 
 "$php_executable" artisan queue:restart
 
-if [[ $("$php_executable" artisan list) =~ "horizon:terminate" ]]; then
+if [[ "$available_commands" =~ "horizon:terminate" ]]; then
     "$php_executable" artisan horizon:terminate
 fi
 
-if [[ $("$php_executable" artisan list) =~ "pulse:restart" ]]; then
+if [[ "$available_commands" =~ "pulse:restart" ]]; then
     "$php_executable" artisan pulse:restart
 fi
